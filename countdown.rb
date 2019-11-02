24ec70e859ff05c20b3d75066f9a3ef82e633422
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-02 23:32:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
-
-def countdown
+def countdown(n)
+  loop do
+    break 'HAPPY NEW YEAR!' if n == 12
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    break if n == 0
+  end
 end
+
+def countdown_with_sleep(n)
+  sleep(n)
+    
+end
+
+countdown(10)
+countdown(12)
+countdown_with_sleep(5)
